1fe6bc6b502f18fbac01aac6ec24f5daf731ad81
Your Name <you@example.com>
2021-01-05 22:46:02 +0800

    文件

--- app.js
+++ app.js
@@ -3,25 +3,25 @@ console.log("ERC720")
 let accounts = [];
 
 if (typeof window.ethereum !== 'undefined') {
-	console.log('MetaMask is installed!');
+    console.log('MetaMask is installed!');
 }
 
 console.log("isMetaMask：" + ethereum.isMetaMask)
 
 $(".enableEthereumButton").click(function () {
+  
+    getAccount()
 
-		getAccount()
-
-	}
+}
 )
 
 
 
 
 async function getAccount() {
-	accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-	const account = accounts[0];
-	$(".showAccount").html(account);
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    const account = accounts[0];
+    $(".showAccount").html(account);
 }
 
 
@@ -40,40 +40,123 @@ let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 console.log("web3", web3)
 
 
-// yzm.sol
+                                     // yzm.sol
 var contractAbi = [
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "a1",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "name",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "phoneNum1",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "peopName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "message4",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_mesName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "_mesAge",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "_idCard",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_lookLike",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_homeAddress",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "_more",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "_famPhone",
+				"type": "uint32"
+			}
+		],
+		"name": "addMessage",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
 	{
 		"inputs": [
 			{
 				"internalType": "string",
 				"name": "_getName",
 				"type": "string"
-			}
-		],
-		"name": "getmassage2",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
 			},
 			{
 				"internalType": "uint32",
-				"name": "",
+				"name": "_peoPhone",
 				"type": "uint32"
 			},
 			{
 				"internalType": "string",
-				"name": "",
-				"type": "string"
-			},
-			{
-				"internalType": "string",
-				"name": "",
+				"name": "message2",
 				"type": "string"
 			}
 		],
-		"stateMutability": "view",
+		"name": "addMoreMes",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
@@ -90,16 +173,26 @@ var contractAbi = [
 			},
 			{
 				"internalType": "string",
-				"name": "_idCard",
+				"name": "_peoType",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "_phoneNum",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "_homeAddr",
 				"type": "string"
 			},
 			{
 				"internalType": "string",
-				"name": "_moreMes",
+				"name": "_password",
 				"type": "string"
 			}
 		],
-		"name": "massage2",
+		"name": "addPeople",
 		"outputs": [
 			{
 				"internalType": "bool",
@@ -114,20 +207,142 @@ var contractAbi = [
 		"inputs": [
 			{
 				"internalType": "string",
-				"name": "",
+				"name": "_getName",
 				"type": "string"
 			}
 		],
-		"name": "people1",
+		"name": "getMessage",
 		"outputs": [
 			{
 				"internalType": "string",
-				"name": "name",
+				"name": "",
 				"type": "string"
 			},
 			{
 				"internalType": "uint32",
-				"name": "age",
+				"name": "",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "",
+				"type": "uint32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_getName",
+				"type": "string"
+			}
+		],
+		"name": "getMoreMes",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "_phoneNum",
+				"type": "uint32"
+			}
+		],
+		"name": "getPeople",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "",
+				"type": "uint32"
+			},
+			{
+				"internalType": "uint32",
+				"name": "",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"name": "messages",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "mesName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "mesAge",
 				"type": "uint32"
 			},
 			{
@@ -137,8 +352,96 @@ var contractAbi = [
 			},
 			{
 				"internalType": "string",
-				"name": "moreMes",
+				"name": "lookLike",
 				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "homeAddress",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "timestamp",
+				"type": "uint256"
+			},
+			{
+				"internalType": "string",
+				"name": "more",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "famPhone",
+				"type": "uint32"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint32",
+				"name": "",
+				"type": "uint32"
+			}
+		],
+		"name": "people1",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "peoName",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "peoAge",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "peoType",
+				"type": "string"
+			},
+			{
+				"internalType": "uint32",
+				"name": "phoneNum",
+				"type": "uint32"
+			},
+			{
+				"internalType": "string",
+				"name": "homeAddr",
+				"type": "string"
+			},
+			{
+				"internalType": "string",
+				"name": "password",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_password",
+				"type": "address"
+			},
+			{
+				"internalType": "uint32",
+				"name": "_phoneNum",
+				"type": "uint32"
+			}
+		],
+		"name": "vertifyAccounts",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
 			}
 		],
 		"stateMutability": "view",
@@ -146,21 +449,22 @@ var contractAbi = [
 	}
 ];
 
-var contract = new web3.eth.Contract(contractAbi, "0xA381A9a9B7406Dbf0275F53bf9381dAFDd942E13");
+var contract = new web3.eth.Contract(contractAbi, "0x9611267eB63c608a899f93e52C1b8e4A53aBC927");
 
 console.log("contract", contract)
 
 
-$("#summitMe").click(function(){
+$("#13").click(function(){
 	alert("success");
 	var account = accounts[0];
 	console.log("提交信息中。。");
-	let _name = $("#name").val();
-	let _age = $("#age").val();
-	let _id = $("#idC").val();
-	let _more = $("#more").val();
-
-	contract.methods.massage2(_name, _age, _id, _more).send({ from: account }).then(
+    let _name = $("#1").val();
+	let _age = $("#2").val();
+	let _peoType = $("#3").val();
+	let _phoneNum = $("#4").val();
+	let _homeAddr= $("#5").val();
+	let _password = $("#6").val();
+	contract.methods.addPeople(_name, _age, _peoType, _phoneNum, _homeAddr, _password).send({ from: account }).then(
 		function (result) {
 			console.log("result", result);
 			if (result.status) {
@@ -173,18 +477,96 @@ $("#summitMe").click(function(){
 })
 
 
-$("#foundMess").click(function () {
+$("#summitMe").click(function () {
 	console.log("查询信息中。。");
-	let _nameFo = $("#nameFo").val();
+	let _mesName = $("#7").val();
+	let _mesAge = $("#8").val();
+	let _idCard = $("#9").val();
+	let _lookLike = $("#10").val();
+	let _homeAddress = $("#11").val();
+	let _more = $("#12").val();
+	let _famPhone = $("#113").val();
 
-	contract.methods.getmassage2(_nameFo).call({ from: accounts[0] }).then(
+
+	contract.methods.addMessage(_mesName, _mesAge, _idCard, _lookLike, _homeAddress, _more, _famPhone).send({ from: accounts[0] }).then(
 		function (result) {
 			console.log("result", result);
-			$("#foName").html(result[0]);
-			$("#foAge").html(result[1]);
-			$("#foId").html(result[2]);
-			$("#foMore").html(result[3]);
+alert("success");
 		}
 	)
 })
 
+
+$("#22").click(function () {
+	alert("success");
+	var account = accounts[0];
+	console.log("提交信息中。。");
+	let _name = $("#21").val();
+
+	contract.methods.getMessage(_name).call({ from: account }).then(
+		function (result) {
+			console.log("result", result);
+			
+			$("#24").html(result[0]);
+			$("#25").html(result[1]);
+			$("#26").html(result[2]);
+			$("#27").html(result[3]);
+			$("#28").html(result[3]);
+			$("#29").html(result[3]);
+			$("#30").html(result[3]);
+		}
+	)
+})
+$("#15").click(function () {
+	alert("success");
+	var account = accounts[0];
+	console.log("提交信息中。。");;
+	let _phoneNum = $("#14").val();
+
+	contract.methods.getPeople(_phoneNum).call({ from: account }).then(
+		function (result) {
+			console.log("result", result);
+			$("#17").html(result[0]);
+			$("#18").html(result[1]);
+			$("#19").html(result[2]);
+			$("#00").html(result[3]);
+		}
+	)
+})
+
+$("#button2").click(function () {
+	alert("success");
+	var account = accounts[0];
+	console.log("提交信息中。。");
+	let _getName = $("#31").val();
+	let _peoPhone = $("#32").val();
+	let message2 = $("#33").val();
+
+
+	contract.methods.addMoreMes(_getName, _peoPhone, message2).send({ from: account }).then(
+		function (result) {
+			console.log("result", result);
+			if (result.status) {
+				alert("添加成功")
+			} else {
+				alert("添加失败")
+			}
+		}
+	)
+})
+$("#35").click(function () {
+	alert("success");
+	var account = accounts[0];
+	console.log("提交信息中。。");
+	let _getName = $("#34").val();
+
+	contract.methods.getMoreMes(_getName).call({ from: account }).then(
+		function (result) {
+			console.log("result", result);
+			$("#38").html(result[0]);
+			$("#lostmessage3").html(result[1]);
+			$("#losttime2").html(result[2]);
+			$("#namen").html(result[3]);
+		}
+	)
+})
--- contracts/system.sol
+++ contracts/system.sol
@@ -9,29 +9,139 @@ contract shiyan {
     //     string massage;
     // }
     
+    //个人信息
     struct people{
-        string name;
-        uint32 age;
+        string peoName;
+        uint32 peoAge;
+        string peoType;
+        uint32 phoneNum;
+        string homeAddr;
+        string password;
+    }
+    
+    mapping(uint32 => people) public people1; //电话号码
+    
+    //失踪人基本信息
+    struct message{
+        string mesName;
+        uint32 mesAge;
         string idCard;
-        string moreMes;
+        string lookLike;
+        string homeAddress;
+        uint256 timestamp;
+        string more;
+        uint32 famPhone;
         
     }
     
-    mapping(string => people) public people1;
-
-    function massage2(string memory _name,uint32 _age,string memory _idCard,string memory _moreMes) public returns (bool) {
-        people1[_name].name= _name;
-        people1[_name].age = _age;
-        people1[_name].idCard = _idCard;
-        people1[_name].moreMes = _moreMes;
-        return true;
+    //搜寻信息
+    struct a{
+        string name;
+        uint32 phoneNum1;
+        string peopName;
+        uint256 timestamp;
+        string message4;
     }
-    function getmassage2(string memory _getName) public view returns (string memory,uint32,string memory,string memory) {
+    
+    mapping(string => a) public a1; //姓名
+    
+    mapping(string => message) public messages;  //姓名
+    
+    //注册账户
+    function addPeople(
+            string memory _name,
+            uint32 _age,
+            string memory _peoType,
+            uint32 _phoneNum,
+            string memory _homeAddr,
+            string memory _password) public returns (bool) {
+            people1[_phoneNum].peoName= _name;
+            people1[_phoneNum].peoAge = _age;
+            people1[_phoneNum].peoType = _peoType;
+            people1[_phoneNum].phoneNum = _phoneNum;
+            people1[_phoneNum].homeAddr = _homeAddr;
+            people1[_phoneNum].password = _password;
+            return true;
+    }
+    
+    //添加失踪人基本信息
+    function addMessage(
+            string memory _mesName,
+            uint32 _mesAge,
+            string memory _idCard,
+            string memory _lookLike,
+            string memory _homeAddress,
+            string memory _more,
+              uint32 _famPhone) public returns(bool){
+          messages[_mesName].mesName = _mesName;
+          messages[_mesName].mesAge = _mesAge;
+          messages[_mesName].idCard = _idCard;
+          messages[_mesName].lookLike = _lookLike;
+          messages[_mesName].timestamp = uint32(block.timestamp);
+          messages[_mesName].more = _more;
+          messages[_mesName].famPhone = _famPhone;
+          
+          return true;
+        }
+    
+    //得到个人信息
+    function getPeople(uint32 _phoneNum) public view returns (string memory,uint32,uint32,string memory) {
         return(
-            people1[_getName].name,
-            people1[_getName].age,
-            people1[_getName].idCard,
-            people1[_getName].moreMes
+            people1[_phoneNum].peoName,
+            people1[_phoneNum].peoAge,
+            people1[_phoneNum].phoneNum,
+            people1[_phoneNum].homeAddr
             );
     }
+    
+    //得到失踪人基本信息
+    function getMessage(string memory _getName) public view returns (string memory,uint32, string memory,string memory,uint256,string memory,uint32) {
+        return(
+            messages[_getName].mesName, 
+            messages[_getName].mesAge ,
+            messages[_getName].idCard ,
+            messages[_getName].lookLike ,
+            messages[_getName].timestamp,
+            messages[_getName].more,
+            messages[_getName].famPhone
+            );
+    }
+    
+    //添加搜寻信息
+    function addMoreMes(string memory _getName,
+    uint32 _peoPhone,
+    string memory message2) public returns(bool){
+        a1[_getName].peopName=  people1[_peoPhone].peoName;
+        a1[_getName].phoneNum1=  _peoPhone;
+        a1[_getName].message4= message2;
+        a1[_getName].timestamp = uint32(block.timestamp);
+        a1[_getName].name = _getName;
+        return true;
+    }
+    
+    //得到搜寻信息
+    function getMoreMes(string memory _getName) public view returns(
+        string memory,
+        uint32,
+        string memory,
+        uint256,
+        string memory){
+            return(
+                a1[_getName].peopName,
+        a1[_getName].phoneNum1,
+        a1[_getName].message4,
+        a1[_getName].timestamp ,
+        a1[_getName].name );
+        
+
+    }
+    
+    //登入注册验证
+    function vertifyAccounts(address _password,uint32 _phoneNum) public view returns(bool){
+                if(keccak256(abi.encodePacked(people1[_phoneNum].password))==keccak256(abi.encodePacked(_password))){
+                    return(true);
+                }
+        return(false);
+    }
+    
 }
